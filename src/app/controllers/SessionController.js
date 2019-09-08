19253e6ecd5068aa2acc3fd014942c487d8e145f
Gabriel Lima <gabriellfsouza@gmail.com>
2019-09-08 06:30:30 -0300

    testing ci deploy

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -36,6 +36,7 @@ class SessionController {
     if (!(await user.checkPassword(password))) {
       return res.status(401).json({ error: 'Password does not match' });
     }
+    
 
     const { id, name, avatar, provider } = user;
     return res.json({
